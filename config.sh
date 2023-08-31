5155641788044a8441e23757c6694c404a0302d5
virmikle <49659083+virmikle@users.noreply.github.com>
2023-08-31 23:33:50 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,33 @@
 
+echo ""
+echo "| Минимальная настройка системы"
+echo ""
+
+echo ""
+read -p ">>> Настройка для Fedora. Продолжаем? (y/n) " choice
+echo ""
+if [ "$choice" == "y" ]; then
+
+sudo dnf upgrade
+sudo dnf update
+sudo dnf install gnome-tweaks
+sudo dnf install steam
+
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward ['<Alt>Shift_L']
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source ['<Shift>Alt_L']
+
+flatpak update
+flatpak install Remmina
+flatpak install onlyoffice
+echo ""
+echo "| Готово!"
+else
+echo "| Отмена!"
+fi
+
+
+
+
+
+
+
