d9e0aa6c545d3653b84835c183da33e175053859
virmikle <49659083+virmikle@users.noreply.github.com>
2023-09-03 00:52:34 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,27 +8,23 @@ read -p ">>> Настройка для Fedora. Продолжаем? (y/n) " cho
 echo ""
 if [ "$choice" == "y" ]; then
 
-sudo dnf upgrade
 sudo dnf update
-sudo dnf check-update
+
 sudo dnf install gnome-tweaks
 sudo dnf install steam
 sudo dnf install code
 
-
-
-
 flatpak update
-flatpak install flathub com.mattjakeman.ExtensionManager
-flatpak install flathub org.remmina.Remmina
-flatpak install flathub org.onlyoffice.desktopeditors
-flatpak install flathub com.jgraph.drawio.desktop
-flatpak install flathub org.kde.krita
-flatpak install flathub org.telegram.desktop
+flatpak install flathub com.mattjakeman.ExtensionManager --noninteractive -y 
+flatpak install flathub org.remmina.Remmina --noninteractive -y 
+flatpak install flathub org.onlyoffice.desktopeditors --noninteractive -y
+flatpak install flathub com.jgraph.drawio.desktop --noninteractive -y
+flatpak install flathub org.kde.krita --noninteractive -y
+flatpak install flathub org.telegram.desktop --noninteractive -y
 
 
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward ['<Alt>Shift_L']
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source ['<Shift>Alt_L']
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt_L']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
 gsettings set org.gnome.desktop.peripherals.mouse speed -0.5
 
 echo ""
