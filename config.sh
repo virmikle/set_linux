c28b32558f51cd0a6149c29d426db49d684e804f
virmikle <49659083+virmikle@users.noreply.github.com>
2023-09-02 23:10:42 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -15,6 +15,8 @@ sudo dnf install steam
 
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward ['<Alt>Shift_L']
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source ['<Shift>Alt_L']
+gsettings set org.gnome.desktop.peripherals.mouse speed -0.5
+
 
 flatpak update
 flatpak install Remmina
